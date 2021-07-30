31142d6c20737d00a74983b4f86f6c61ee79ee9f
Álvaro Emparán <alvaro.emparan@gmail.com>
2021-07-30 16:19:02 -0400

    update

--- spa/src/app/components/home/home.component.ts
+++ spa/src/app/components/home/home.component.ts
@@ -13,14 +13,27 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.Randomw());
+    this.ReadDataFromArray();
   }
 
   public Randomw(){
-
     return Math.floor(Math.random() * 1000);
-
-
   }
 
+  public ReadDataFromArray(){
+
+    let found:string = null;
+    const array1 = [{'GlsDes':'uNo','DlsVal':'1'},{'GlsDes':'cuatro','DlsVal':'4'},{'GlsDes':'cinCo','DlsVal':'5'}];
+
+    found = array1.find(el => el.GlsDes.toUpperCase() == 'UNO').DlsVal;
+    console.log(found);
+
+    found = array1.find(el => el.GlsDes.toUpperCase() == 'CUATRO').DlsVal;
+    console.log(found);
+    
+    found = array1.find(el => el.GlsDes.toUpperCase() == 'CINCO').DlsVal;
+    console.log(found);
+
+  }
 
 }
